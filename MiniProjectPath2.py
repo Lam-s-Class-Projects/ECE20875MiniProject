e75aa73ffa86d165b2761f5e55e70c7127b83058
HaiLamLe <94097074+HaiLamLe@users.noreply.github.com>
2023-12-21 23:14:25 -0500

    Add files via upload

---
+++ MiniProjectPath2.py
@@ -0,0 +1,19 @@
+# Path 2: Bike traffic
+# The nyc_bicycle_counts_2016.csv gives information on bike traffic across a number of bridges in New York City.
+# In this path, the analysis questions we would like you to answer are as follows:
+#
+# 1. You want to install sensors on the bridges to estimate overall traffic across all the bridges. But you only
+# have enough budget to install sensors on three of the four bridges. Which bridges should you install the sensors
+# on to get the best prediction of overall traffic?
+#
+# 2. The city administration is cracking down on helmet laws, and wants to deploy police officers on days with high
+# traffic to hand out citations. Can they use the next day's weather forecast(low/high temperature and precipitation)
+# to predict the total number of bicyclists that day?
+#
+# 3. Can you use this data to predict what day (Monday to Sunday) is today based on the number of bicyclists on the bridges?
+#
+# readme-nyc_bicycle_counts_2016.pdf details the information contained in the data field.
+
+
+# Team: Hai Lam Le & Harini Subramanian
+# To answer each of the questions above, we created and organized 3 files called problem1.py, problem2.py, problem3.py
